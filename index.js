53964f447fd1b1dcdbe4a961a9c804cc3df28250
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-10-31 23:10:12 +0600

    updated

--- index.js
+++ index.js
@@ -88,9 +88,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello from Tour Server");
 });
-app.get("/hello", (req, res) => {
-  res.send("Fahim own Server");
-});
 
 app.listen(port, () => {
   console.log(`Listening Tour Server to port:${port}`);
