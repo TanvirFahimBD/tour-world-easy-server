51821f8589bbd0e70076e5005c8f6651a384c7e5
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-10-31 20:36:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourWorld");
+    const servicesCollection = database.collection("bookings");
+
+    //POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
+
+    //GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //Get single service info view on server side
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific id ", id);
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    //Delete API
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // UPDATE API
+    app.put("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedService = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          name: updatedService.name,
+          price: updatedService.price,
+          img: updatedService.img,
+          description: updatedService.description,
+        },
+      };
+      const result = await servicesCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      console.log("Updating services", id);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Tour Server");
+});
+
+app.listen(port, () => {
+  console.log(`Listening Tour Server to port:${port}`);
+});
